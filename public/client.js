33ad896588aff3e0d020c4fb6714b47408853c42
Brandon Baker <bakerbrandond@gmail.com>
2022-04-16 13:12:59 -0500

    use provided counterkey

--- public/client.js
+++ public/client.js
@@ -1,9 +1,12 @@
-function getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-}
+const key = "1ccb732e-b55a-4404-ad3f-0f99c02fe44e"
 const button = document.getElementById('apiButton');
 button.addEventListener('click', function(e) {
-    x = getRandomInt(64)
-    document.getElementById('textField').innerHTML = `API hit count: ${x}`;
     console.log('button was clicked');
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.countapi.xyz/hit/${key}`);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        document.getElementById('textField').innerHTML = `API hit count: ${this.response.value}`;
+    }
+    xhr.send();
 });
