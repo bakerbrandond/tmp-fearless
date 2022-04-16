39fc24f3ef132141083a36572fe77c37b77c9209
Brandon Baker <bakerbrandond@gmail.com>
2022-04-16 12:37:29 -0500

    temporary: populate string with random int on button press

--- public/client.js
+++ public/client.js
@@ -1,4 +1,9 @@
+function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+}
 const button = document.getElementById('apiButton');
 button.addEventListener('click', function(e) {
+    x = getRandomInt(64)
+    document.getElementById('textField').innerHTML = `API hit count: ${x}`;
     console.log('button was clicked');
 });
