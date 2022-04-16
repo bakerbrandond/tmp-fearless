8fb3726a43993b57dea8011d9d7cc5ad2035ff33
Brandon Baker <bakerbrandond@gmail.com>
2022-04-16 12:23:48 -0500

    add button click

---
+++ public/client.js
@@ -0,0 +1,4 @@
+const button = document.getElementById('apiButton');
+button.addEventListener('click', function(e) {
+    console.log('button was clicked');
+});
--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+// serve files from the public directory
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // request to handle undefined or all other routes
 app.get('*', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, (err) => {
